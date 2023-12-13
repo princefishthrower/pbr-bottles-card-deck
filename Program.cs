1ef48cc527f4ea33983f9d7e6a789757b8a267d9
Chris Frewin <frewin.christopher@gmail.com>
2023-12-12 20:36:52 -0500

    initial commit - surprised i didn't make a quick repo for this from years ago

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pbr_bottles_card_deck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Set up card ids, number of trials, and draw counts
+            var cardIDs = Enumerable.Range(1, 52).ToArray();
+            var trials = 10000;
+            var drawCounts = new List<int>();
+
+            // Enter experiment loop
+            for (var i = 0; i < trials; i++)
+            {
+                var ourCards = new List<int>();
+                var drawCount = 0;
+
+                // Continue to draw cards until we have all cards to make a deck
+                while (ourCards.Sum() != cardIDs.Sum())
+                {
+                    var random = new Random();
+                    var index = random.Next(cardIDs.Count());
+                    var cardIDDrawn = cardIDs[index];
+
+                    if (!ourCards.Contains(cardIDDrawn))
+                    {
+                        ourCards.Add(cardIDDrawn);
+                    }
+
+                    drawCount++;
+                }
+                drawCounts.Add(drawCount);
+                Console.WriteLine($"Done with experiment. Draw count needed was {drawCount}");
+            }
+
+            Console.WriteLine($"Average draw count needed over all experiments was {drawCounts.Average()}");
+            Console.WriteLine($"Minimum draw count needed over all experiments was {drawCounts.Min()}");
+            Console.WriteLine($"Maximum draw count needed over all experiments was {drawCounts.Max()}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# PBR Bottles Card Deck!
+
+Calculates on average, how many beers you would need to drink to complete a card deck (assuming random repeats across all 52 cards).
+
+Run with:
+
+```shell
+dotnet run
+```
